e0b07f4bbd5e09bcebf14576ee6a613f6c76dddf
Roger <worth22focus3000@gmail.com>
2024-01-18 16:41:31 +0800

    feat: add settings page (display User info and sign out button)

---
+++ app/(protected)/settings/page.tsx
@@ -0,0 +1,25 @@
+import { auth, signOut } from "@/auth";
+
+const SettingsPage = async () => {
+  const session = await auth();
+
+  return (
+    <>
+      <div>Settings Page</div>
+      <div>{JSON.stringify(session)}</div>
+      <div>
+        <form
+          action={async () => {
+            "use server";
+
+            await signOut();
+          }}
+        >
+          <button type="submit">Sign Out</button>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default SettingsPage;
